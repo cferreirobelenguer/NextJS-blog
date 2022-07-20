f770980f5f444e16f38240cc02a1c76114e21511
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-07-20 19:22:35 +0200

    componente blog

--- components/blog.js
+++ components/blog.js
@@ -15,7 +15,7 @@ const Blog=()=>{
     return(
         <div className="blog">
             <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto">
+                <div className="container px-10 py-24  mx-auto">
                     <div className="flex flex-wrap -m-4" id="parrafo">
                         {mostrarDatos()}
                         {data.map((i) =>{
