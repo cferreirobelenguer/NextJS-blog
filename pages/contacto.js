32dece7f37aca5f7b6849daea01e375542a063e4
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-07-23 12:05:50 +0200

    formulario

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,12 +1,30 @@
 //url http/localhost:3000/contacto
-import React from "react";
+import React, {useRef, useState} from "react";
 import Header from '../components/portada'
 import Footer from '../components/footer'
 import FotoContacto from '../assets/images/contact.png'
 import Image from 'next/image'
 import huella from '../assets/images/huella.svg'
 
+    var mensajeDeEnvio;
+
 const Contacto=()=>{
+    const [mensajeEnvio, setMensajeEnvio]=useState("");
+    var name=useRef(null);
+    var email=useRef(null);
+    var message=useRef(null);
+    
+    //se cogen los datos del formularo
+    const envioFormulario=(e)=>{
+        //para que no se recargue la página cada vez que se de a submit
+        e.preventDefault();
+        var nombre=name.current.value;
+        var correo=email.current.value;
+        var mensaje=message.current.value;
+        mensajeDeEnvio="Gracias por contactarnos "+nombre+" en breves nos pondremos en contacto contigo al email "+correo+", saludos de parte de Blog Dog "
+        console.log(mensajeDeEnvio)
+        setMensajeEnvio(mensajeDeEnvio)
+    }
     return(
         <div>
             <Header/>
@@ -17,7 +35,7 @@ const Contacto=()=>{
                             <Image
                                     src={FotoContacto}
                                     alt="imagen contacto"
-                                    width={450}
+                                    width={500}
                                     height={400}
                             />
                     </div>
@@ -31,20 +49,25 @@ const Contacto=()=>{
                                         height={25}
                                     />&nbsp;Contáctanos</h3>
                             <p className="leading-relaxed mb-5 text-gray-600">¿Tienes dudas, necesitas asesoramiento? Estamos para ayudarte</p>
+                                <form onSubmit={envioFormulario}>
                                 <div className="relative mb-4">
                                     <label htmlFor="name" className="leading-7 text-sm text-gray-600">Nombre</label>
-                                    <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                                    <input type="text" id="name" ref={name} name="name" className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                                 </div>
                                 <div className="relative mb-4">
                                     <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                                    <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                                    <input type="email" id="email" ref={email} name="email" className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                                 </div>
                                 <div className="relative mb-4">
                                     <label htmlFor="message" className="leading-7 text-sm text-gray-600">Mensaje</label>
-                                    <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                    <textarea id="message" name="message" ref={message} className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                 </div>
-                                <button className="text-white bg-sky-300 border-0 py-2 px-6 focus:outline-none hover:bg-sky-400 rounded text-lg">Enviar</button>
+                                <button className="text-white bg-sky-300 border-0 py-2 px-6 focus:outline-none hover:bg-sky-400 rounded text-lg" onSubmit={envioFormulario}>Enviar</button>
+                                </form>
                             <p className="text-xs text-gray-500 mt-3">Protegemos tu privacidad de datos</p>
+                            <br>
+                            </br>
+                            <p className="text-xs text-gray-500 mt-3">{mensajeEnvio}</p>
                         </div>
                     </div>
                     </section>
